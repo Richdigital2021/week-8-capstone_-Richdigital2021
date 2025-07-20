6f0bddbeb6862277f72926ad944e7f8fb64905ad
Richdigital2021 <creativegraphicshub@gmail.com>
2025-07-21 00:37:09 +0100

    modified App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,7 @@ import Login from "@/pages/Login";
 import Signup from "@/pages/Signup";
 import Dashboard from "@/pages/Dashboard";
 import AdminDashboard from "@/pages/AdminDashboard";
-// import Navbar from "@/components/Navbar";
 import ContactForm from "@/components/ContactForm";
-//import Footer from "@/components/Footer";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -22,12 +20,11 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admindashboard" element={<AdminDashboard />} />
         <Route
           path="/protected"
           element={
             <ProtectedRoute>
-              <Dashboard />
+              <AdminDashboard />
             </ProtectedRoute>
           }
         />
